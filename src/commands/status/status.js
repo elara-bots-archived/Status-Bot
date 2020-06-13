1c55f89f7a780822c64afe2b78c38cd8c7f26b23
Sora <kiuy.or@icloud.com>
2020-06-13 13:39:56 +0200

    replace forEach per for

--- src/commands/status/status.js
+++ src/commands/status/status.js
@@ -1,5 +1,5 @@
-const {Command} = require('great-commando'), 
-{MessageEmbed} = require('discord.js');
+const {Command} = require('great-commando'),
+    {MessageEmbed} = require('discord.js');
 module.exports = class Status extends Command {
     constructor(client){
         super(client, {
@@ -13,30 +13,31 @@ module.exports = class Status extends Command {
     };
     async run(message){
         let online = [], idle = [], dnd = [], offline = [];
-        await this.client.watch.forEach(async user => {
+         for (let user of this.client.watch){
             let object = await this.client.users.cache.get(user.userID);
             switch(object.presence.status){
                 case "online":
-                online.push(`${object.username}`)
-                break;
+                    online.push(`${object.username}`)
+                    break;
                 case "idle":
-                idle.push(object.username)
-                break;
+                    idle.push(object.username)
+                    break;
                 case "dnd":
-                dnd.push(object.username)
-                break;
+                    dnd.push(object.username)
+                    break;
                 case 'offline':
-                offline.push(object.username)
-                break;
+                    offline.push(object.username)
+                    break;
             }
-        });
-        let e = new MessageEmbed()
-        .setTitle(`Status`)
-        .setColor(message.member.displayColor).setTimestamp()
-        if(offline.length !== 0) e.addField(`**Offline**`, offline.join('\n'))
-        if(online.length !== 0) e.addField(`Online`, online.join('\n'))
+        }
+
+        let e = new MessageEmbed();
+        e.setTitle(`Status`);
+        e.setColor(message.member.displayColor).setTimestamp();
+        if(offline.length !== 0) e.addField(`**Offline**`, offline.join('\n'));
+        if(online.length !== 0) e.addField(`Online`, online.join('\n'));
         if(idle.length !== 0) e.addField(`Idle`, idle.join('\n'));
-        if(dnd.length !== 0) e.addField(`Dnd`, dnd.join('\n'))
+        if(dnd.length !== 0) e.addField(`Dnd`, dnd.join('\n'));
         return message.channel.send(e)
     }
 }
