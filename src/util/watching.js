73ec7731fc7520d019cf3cf999020653860d53a5
Elara Services <43923163+Elara-Discord-Bots@users.noreply.github.com>
2021-09-25 10:13:38 -0700

    Update watching.js

--- src/util/watching.js
+++ src/util/watching.js
@@ -5,21 +5,13 @@
 // I ain't responsible for idiots who fuck it up, if you fuck it up don't complain.. I warned you to not mess with this file.
 
 
+const list = [];
 
-
-
-
-const List = [];
-
-module.exports.add = (userID, webhook, guildID, alert = {role: "", enabled: false}) => {
-    let obj = {
-        userID: userID,
-        webhook: webhook,
-        guildID: guildID,
-        alert: alert
-    };
-    List.push(obj);
-    return obj
+module.exports = {
+    add: (userID, webhook, guildID, alert = { role: "", enabled: false }) => {
+        let obj = { userID, webhook, guildID, alert };
+        list.push(obj);
+        return obj
+    },
+    list
 };
-
-module.exports.list = List;
