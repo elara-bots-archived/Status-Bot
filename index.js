9652ec44b0ec08897676d2bb60b362ce571f3d3b
Elara Services <43923163+Elara-Discord-Bots@users.noreply.github.com>
2021-09-25 10:23:31 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+require("dotenv").config();
+
 const Bot = require("./src/index"),
-      {add} = require("./src/util/watching");
+    { add } = require("./src/util/watching");
 
 
 new Bot({
     owners: [], // YOUR USER ID IN HERE
-    prefix: "?", // DEFAULT IS ?
-    token: "", // YOUR BOT TOKEN  (DO NOT SHARE WITH ANYONE)
+    prefix: process.env.PREFIX || "?", // DEFAULT IS ?
+    token: process.env.BOT_TOKEN, // YOUR BOT TOKEN  (DO NOT SHARE WITH ANYONE)
     // embed: {} // on: {}, off: {} // refer to the readme
     watch: [
         // The user/bot's ID, The full webhook URL, the guildID that you want the bot to watch for, the roleID and if you want it enabled.
